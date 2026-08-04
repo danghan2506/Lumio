1a76ed0ff5a687e0bd02a749b638a2798447f051
danghan2506 <danghan1213@gmail.com>
2026-08-04 15:34:10 +0700

    feat(assets): register lumi-welcome image in constants

--- constants/images.ts
+++ constants/images.ts
@@ -1,5 +1,8 @@
 import lumiMascot from '@/assets/images/lumi_mascot.jpg';
+import lumiWelcome from '@/assets/images/lumi-welcome.png';
 
 export const images = {
   mascot: lumiMascot,
+  welcome: lumiWelcome,
 };
+
