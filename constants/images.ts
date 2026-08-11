ec61b3ede1fb8d53c04354438912580b24a9f9e3
danghan2506 <danghan1213@gmail.com>
2026-08-11 22:22:07 +0700

    feat(assets): register lumi mascot image assets in constants/images.ts

--- constants/images.ts
+++ constants/images.ts
@@ -1,7 +1,13 @@
 import lumiMascot from "@/assets/images/lumi_mascot.jpg";
 import lumiWelcome from "@/assets/mascot/lumi-tutor.png";
+import lumiTutor from "@/assets/mascot/lumi-tutor.png";
+import lumiDefault from "@/assets/mascot/lumi-default.png";
+import lumiCelebration from "@/assets/mascot/lumi-celebration.png";
 
 export const images = {
   mascot: lumiMascot,
   welcome: lumiWelcome,
+  lumiTutor,
+  lumiDefault,
+  lumiCelebration,
 };
